a12648c7cde2710d2fbfd900201c784fa142e574
A-Tetarwal <andashishwill@gmail.com>
2024-02-02 22:09:52 +0530

    update repository

---
+++ README.md
@@ -0,0 +1,31 @@
+# JavaScript Learning Repository
+
+Welcome to my JavaScript Learning Repository! This repository is a collection of resources, code snippets, and projects aimed at helping beginners and intermediate learners grasp the fundamentals and advanced concepts of JavaScript programming.
+
+## Table of Contents
+
+1. Getting Started
+2. Topics Covered
+3. Code Snippets
+4. Projects
+5. Contributing
+6. Resources
+
+
+## Topics Covered
+
+The repository covers a wide range of JavaScript topics, including:
+
+- Variables and Data Types
+- Control Flow (if statements, loops)
+- Functions and Scope
+- Objects and Arrays
+- Asynchronous JavaScript (Promises, Async/Await)
+- DOM Manipulation
+- Events and Event Handling
+- AJAX and Fetch API
+- ES6+ Features
+- Testing with Jest
+
+
+Happy coding! 🚀
---
+++ first.js
@@ -0,0 +1,56 @@
+console.log("Ashish Tetarwal welcomes you");
+console.log("This is The Yard Project");
+
+name="Ashish"
+console.log(name);
+
+let fullName= "Tony Stark";
+const age=24;
+let price = 99.99;
+const radius=14;
+a=null;
+isFollow = true;
+console.log(fullName);
+
+let x=BigInt("123");
+let y= Symbol("Hello");
+
+const student = {
+    fullName: "jaani",
+    age: 20,
+    gpa:8.2,
+    isPass: true
+};
+
+student["age"]=student["age"]+1;
+student["fullName"]= "Jaani Veere";
+
+console.log(student["age"]);
+console.log(student.fullName);
+console.log(student.gpa);
+
+
+// creating an object for a product listed on eCommerce website
+const pen={
+    Name: "Parker Jotter Standard CT Ball Pen",
+    colour: "black",
+    Rating: "4/5, 7000+ Ratings",
+    Offer_Price_in_Rs: 270,
+    Original_Price: 285,
+    isDeal: true
+};
+
+console.log(pen);
+
+// creating a const object called "profile" to store a given profile information
+const profile= {
+    Name: "ShradhaKhapra",
+    isVerified: true,
+    Posts: 195,
+    followers: 569000,
+    following: 4,
+    User: "Shradha Khapra",
+    tag: "Enterpreneur",
+    About: "Apna College | Ex-Microsoft, DRDO"
+};
+console.log(profile);
