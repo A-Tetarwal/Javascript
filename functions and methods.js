a0d8fa35b92f9400b973ab629b19a2e2190931e3
A-Tetarwal <andashishwill@gmail.com>
2024-02-14 23:40:46 +0530

    update in all html files

---
+++ functions and methods.js
@@ -0,0 +1,27 @@
+console.log('hello!');
+
+'abc'.toUpperCase();
+[1,2,3].push(4);
+// here all these .log(), .toUpperCase(), .push() are functions, () -> is a parenthesis, it invokes the functions, ismein hum input dete hain
+
+/* 
+function definition -> 
+
+function functionName(){ 
+    // do some work
+}
+
+function functionName(parameter1, parameter2, ...){
+    // do some work
+}
+
+
+functon call ->
+
+functionName();
+ */
+
+function printSomething(){
+    console.log('hello brother')
+    console.log(`hello peter`)
+}
