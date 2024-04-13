484fcd111849431a094ba528fb3161252857a434
A-Tetarwal <andashishwill@gmail.com>
2024-04-13 15:27:00 +0530

    added random.js

---
+++ learning/random.js
@@ -0,0 +1,14 @@
+
+
+for (let count = 0; count < 5; count++) {
+    console.log("Ashish");   
+}
+
+
+// for-of loop for string and arrays
+let rom = "randomAccessMemory"
+
+for (const i of rom) {
+    console.log(i);
+}
+console.log(rom.length)
