e87367d67734ab38868930d8cd7491ae243a05d6
A-Tetarwal <andashishwill@gmail.com>
2024-05-25 16:13:30 +0530

    objects3

---
+++ javascript2/object_part3.js
@@ -0,0 +1,23 @@
+// destructure of object
+
+const course = {
+    cname: "Javascript",
+    price: "999",
+    courseInstructor: "Hitesh Choudhary"
+}
+
+const {courseInstructor: instructor} = course;
+console.log(instructor);
+
+// API: in form of JSON
+// {
+//     "name": "Ashish",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+ [
+    {},
+    {},
+    {}
+ ]
