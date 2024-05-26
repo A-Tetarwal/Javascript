53273153433892d10269265d9d665ed22fef7625
A-Tetarwal <andashishwill@gmail.com>
2024-05-26 08:57:59 +0530

    changes

---
+++ javascript2/part2/functions/functions_1.js
@@ -0,0 +1,21 @@
+function sayMyName(){
+    console.log("Ashish");
+}
+
+sayMyName  // ye reference hai function ka
+sayMyName() // ye execution hai
+
+// declaring add function
+
+// function addTwoNums(num1, num2){
+//     console.log(num1+num2);
+// }
+
+
+// again
+
+function addTwoNums(num1, num2){
+    console.log(num1+num2);
+}
+const result = addTwoNums(4,5)
+console.log("result: ", result); 
