de7a3c3df6c33bd97418ded692b2ca27c91e822e
A-Tetarwal <andashishwill@gmail.com>
2024-07-02 21:16:43 +0530

    ES6

---
+++ javascript2/ES6-JS/es6.js
@@ -0,0 +1,15 @@
+// arrow functions- fat arrow fnc
+const a = () => {}
+
+const b = (param) => {} // ek variable  ho to brackets ht skte hain
+b(12)
+// jaise
+const c = param => {}
+c('hello')
+
+// lekin agar ek se jyada ho to bracket lgana jruri hai
+const d = (p1, p2) => {}
+
+// arrow fn with impicit return
+const e = () => 12
+console.log(e());
