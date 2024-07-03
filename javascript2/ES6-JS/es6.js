bc64b55fcf939e9393fda0ec9a405cbda99d3fd6
A-Tetarwal <andashishwill@gmail.com>
2024-07-03 23:51:47 +0530

    update

--- javascript2/ES6-JS/es6.js
+++ javascript2/ES6-JS/es6.js
@@ -40,3 +40,19 @@ function abcd(a,b,c,...d){
 }
 
 abcd(1,2,3,4,6,6,7,9);
+
+
+// ----- destructuring -----
+
+const ar = [1,2,3];
+const [a1, b1] = ar; // isse array nahin ban rha hai, isse do naye variables bn rhe hain jo pehli do values lenge 'ar' array mein se
+const [a2,b2,c2] = ar; // three new variables to store three values of 'ar'
+const [a4,b4,c4,d4] = ar; // yahan pr d4 undefined rhega kyuki array me to values teen hi hain
+const [a3,,c3] = ar; // isme double comma lgane se ek value skip ho gyi hai aur first and third value mil jayegi inn variables ko
+const [,,a6] = ar // yaha pe last value mil gayi hai
+
+
+const obj = {name: 'Ashish', age: 20};
+const {age} = obj; // ye obj me se age naame ka variable nikal lega
+
+
